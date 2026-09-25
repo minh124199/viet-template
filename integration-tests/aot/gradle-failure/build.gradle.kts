d3cd3a5f2e72d689a70aec96686a2c31f1de0981
M18 Implementation <m18-tck@viet-template>
2026-09-25 13:42:12 +0700

    chore(release): prepare 1.0.0-RC2 metadata

--- integration-tests/aot/gradle-failure/build.gradle.kts
+++ integration-tests/aot/gradle-failure/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("io.github.minh124199.viet-template") version "1.0.0-RC1"
+    id("io.github.minh124199.viet-template") version "1.0.0-RC2"
 }
 
 repositories {
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    implementation("io.github.minh124199:viet-template-api:1.0.0-RC1")
-    implementation("io.github.minh124199:viet-template-runtime:1.0.0-RC1")
-    implementation("io.github.minh124199:viet-template-vtl-interpreter:1.0.0-RC1")
+    implementation("io.github.minh124199:viet-template-api:1.0.0-RC2")
+    implementation("io.github.minh124199:viet-template-runtime:1.0.0-RC2")
+    implementation("io.github.minh124199:viet-template-vtl-interpreter:1.0.0-RC2")
 }
